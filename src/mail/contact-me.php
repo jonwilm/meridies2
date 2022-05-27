cc8c1cb414e648622d15cfec782020e496344165
jonwilm <jparedes84@gmail.com>
2022-05-27 01:51:05 -0400

    Ajustes

--- src/mail/contact-me.php
+++ src/mail/contact-me.php
@@ -13,14 +13,17 @@
   $mail = $_POST['mail'];
   $consulta = $_POST['consulta'];
 
-  $to = 'jparedes84@gmail.com';
+  $to = 'pmpgarciatedesco@gmail.com';
   $email_subject = "Contacto desde el Sitio Web - Meridies 2";
-  $email_body = "Has recibido un nuevo correo desde el sitio web Meridies 2.\n\n"."
-  Nombre: $name\n
-  Email: $mail\n
-  Consulta: $consulta\n";
+  $email_body  = '<p><strong>Has recibido un nuevo correo desde el sitio web Meridies 2.</strong></p>';
+  $email_body .= '<p><strong>Nombre: </strong>' . $name . '</p>';
+  $email_body .= '<p><strong>Email: </strong>' . $mail . '</p>';
+  $email_body .= '<p><strong>Consulta: </strong></p>';
+  $email_body .= '<p>' . $consulta . '</p>';
 
-  $headers = "From: info@meridies2.com\n";
+  $headers = 'MIME-Version: 1.0' . "\r\n";
+  $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+  $headers .= "From: info@meridies2.com\n";
   $headers .= "Reply-To: $mail";
 
   $a = mail($to,$email_subject,$email_body,$headers);
